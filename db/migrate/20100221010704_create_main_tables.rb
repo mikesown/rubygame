fea265697f8c931e7c395fd520a6fdd6a6027a06
Michael Miller <mike@resnet-45-230.dorm.utexas.edu>
2010-03-11 19:53:03 -0600

    Added forigner for sqlite foreign key support. Everything's working.

--- db/migrate/20100221010704_create_main_tables.rb
+++ db/migrate/20100221010704_create_main_tables.rb
@@ -2,53 +2,63 @@ class CreateMainTables < ActiveRecord::Migration
   def self.up
     create_table "games", :force => true do |t|
       t.column :name,           :string, :null => false
-      t.column :user_id,         :integer, :null => false
-      t.column :created_at,    :datetime
-      t.column :saved,           :text
-      t.column :class_name,   :string, :null => false
-      t.column :filename,        :string, :null => false
-      t.column :size,              :integer
-      t.column :content_type, :string
+      t.references :user, :null => false
+      t.foreign_key :users,      :column => :user_id, :null => false
+      t.column :created_at,     :datetime
+      t.column :saved,          :text
+      t.column :class_name,     :string, :null => false
+      t.column :filename,       :string, :null => false
+      t.column :size,           :integer
+      t.column :content_type,   :string
     end
     
     create_table "players", :force => true do |t|
       t.column :name,           :string, :null => false
-      t.column :game_id,       :integer, :null => false
+      t.references :game
+      t.foreign_key :games,      :column => :game_id, :null => false
       t.column :required,       :boolean, :null => false, :default => 1
     end
     
     create_table "agents", :force => true do |t|
       t.column :name,            :string, :null => false
-      t.column :user_id,         :integer, :null => false
-      t.column :created_at,    :datetime
-      t.column :saved,           :text
-      t.column :class_name,    :string, :null => false
-      t.column :filename,        :string, :null => false
-      t.column :size,              :integer
-      t.column :content_type, :string
+      t.references :user
+      t.foreign_key :users,      :column => :user_id, :null => false
+      t.column :created_at,     :datetime
+      t.column :saved,          :text
+      t.column :class_name,     :string, :null => false
+      t.column :filename,       :string, :null => false
+      t.column :size,           :integer
+      t.column :content_type,   :string
     end
     
     create_table "agents_games", :force => true, :id => false do |t|
-      t.column :agent_id,        :integer, :null => false
-      t.column :game_id,        :integer, :null => false
+      t.references :agent
+      t.foreign_key :agents,     :column => :agent_id, :null => false
+      t.references :game
+      t.foreign_key :games,      :column => :game_id, :null => false
     end
     
     add_index :agents_games, [:agent_id, :game_id], :unique => true
     
     create_table "results", :force => true do |t|
-      t.column :game_id,       :integer, :null => false
-      t.column :user_id,        :integer, :null => false
+      t.references :game
+      t.foreign_key :games,      :column => :game_id, :null => false
+      t.references :user
+      t.foreign_key :users,      :column => :user_id, :null => false
       t.column :created_at,   :datetime, :null => false
-      t.column :result,          :text, :null => false
-      t.column :saved,          :text
+      t.column :result,       :text, :null => false
+      t.column :saved,        :text
     end
     
     create_table "participants", :force => true, :id => false do |t|
-      t.column :result_id,      :integer, :null => false
-      t.column :agent_id,      :integer, :null => false
-      t.column :player_id,      :integer, :null => false
+      t.references :result
+      t.foreign_key :results,    :column  => :result_id, :null => false
+      t.references :agent
+      t.foreign_key :agents,     :column => :agent_id, :null => false
+      t.references :player
+      t.foreign_key :players,    :column => :player_id, :null => false
       t.column :score,          :integer
-      t.column :result,          :text
+      t.column :result,         :text
       t.column :winner,         :boolean
       t.column :saved,          :text
     end
--- db/schema.rb
+++ db/schema.rb
@@ -9,74 +9,72 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100224000112) do
+ActiveRecord::Schema.define(:version => 20100221010704) do
 
   create_table "agents", :force => true do |t|
-    t.string   "name",         :default => "", :null => false
-    t.integer  "user_id",      :default => 0,  :null => false
+    t.string   "name",         :null => false
+    t.integer  "user_id"
     t.datetime "created_at"
     t.text     "saved"
-    t.string   "class_name",   :default => "", :null => false
-    t.string   "filename",     :default => "", :null => false
+    t.string   "class_name",   :null => false
+    t.string   "filename",     :null => false
     t.integer  "size"
     t.string   "content_type"
+    t.foreign_key "users", :column => "user_id", :dependent => nil
   end
 
-  add_index "agents", ["user_id"], :name => "fk_agents_users"
-
   create_table "agents_games", :id => false, :force => true do |t|
-    t.integer "agent_id", :default => 0, :null => false
-    t.integer "game_id",  :default => 0, :null => false
+    t.integer "agent_id"
+    t.integer "game_id"
+    t.foreign_key "agents", :column => "agent_id", :dependent => nil
+    t.foreign_key "games", :column => "game_id", :dependent => nil
   end
 
   add_index "agents_games", ["agent_id", "game_id"], :name => "index_agents_games_on_agent_id_and_game_id", :unique => true
-  add_index "agents_games", ["game_id"], :name => "fk_agents_games_games"
 
   create_table "games", :force => true do |t|
-    t.string   "name",         :default => "", :null => false
-    t.integer  "user_id",      :default => 0,  :null => false
+    t.string   "name",         :null => false
+    t.integer  "user_id",      :null => false
     t.datetime "created_at"
     t.text     "saved"
-    t.string   "class_name",   :default => "", :null => false
-    t.string   "filename",     :default => "", :null => false
+    t.string   "class_name",   :null => false
+    t.string   "filename",     :null => false
     t.integer  "size"
     t.string   "content_type"
+    t.foreign_key "users", :column => "user_id", :dependent => nil
   end
 
-  add_index "games", ["user_id"], :name => "fk_games_users"
-
   create_table "participants", :id => false, :force => true do |t|
-    t.integer "result_id", :default => 0, :null => false
-    t.integer "agent_id",  :default => 0, :null => false
-    t.integer "player_id", :default => 0, :null => false
+    t.integer "result_id"
+    t.integer "agent_id"
+    t.integer "player_id"
     t.integer "score"
     t.text    "result"
     t.boolean "winner"
     t.text    "saved"
+    t.foreign_key "agents", :column => "agent_id", :dependent => nil
+    t.foreign_key "players", :column => "player_id", :dependent => nil
+    t.foreign_key "results", :column => "result_id", :dependent => nil
   end
 
-  add_index "participants", ["agent_id"], :name => "fk_participants_agents"
-  add_index "participants", ["player_id"], :name => "fk_participants_players"
   add_index "participants", ["result_id", "agent_id", "player_id"], :name => "index_participants_on_result_id_and_agent_id_and_player_id", :unique => true
 
   create_table "players", :force => true do |t|
-    t.string  "name",     :default => "",   :null => false
-    t.integer "game_id",  :default => 0,    :null => false
+    t.string  "name",                       :null => false
+    t.integer "game_id"
     t.boolean "required", :default => true, :null => false
+    t.foreign_key "games", :column => "game_id", :dependent => nil
   end
-
-  add_index "players", ["game_id"], :name => "fk_players_games"
 
   create_table "results", :force => true do |t|
-    t.integer  "game_id",    :default => 0, :null => false
-    t.integer  "user_id",    :default => 0, :null => false
-    t.datetime "created_at",                :null => false
-    t.text     "result",                    :null => false
+    t.integer  "game_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.text     "result",     :null => false
     t.text     "saved"
+    t.foreign_key "games", :column => "game_id", :dependent => nil
+    t.foreign_key "users", :column => "user_id", :dependent => nil
   end
-
-  add_index "results", ["game_id"], :name => "fk_results_games"
-  add_index "results", ["user_id"], :name => "fk_results_users"
 
   create_table "users", :force => true do |t|
     t.string   "login"
